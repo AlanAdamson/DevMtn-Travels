a92f0c14cef02f440342ef06b2bf80e0c611b9aa
AlanAdamson <alan@adamson.im>
2016-10-06 19:05:49 -0600

    finished

--- js/bookedCtrl.js
+++ js/bookedCtrl.js
@@ -1,11 +1,18 @@
 angular.module('devmtnTravel').controller('bookedCtrl', function($scope, mainSrv, $stateParams){
+  var trvInfo = mainSrv.travelInfo;
   if ($stateParams.id === '112233') {
-    $scope.blah = mainSrv.travelInfo[0];
+    $scope.blah = trvInfo[0];
+    $scope.backgrnd = trvInfo[0].image;
+    $scope.travelInfo =  trvInfo[0];
   }
   else if ($stateParams.id === '1122343') {
-    $scope.blah = mainSrv.travelInfo[1];
+    $scope.blah = trvInfo[1];
+    $scope.backgrnd = trvInfo[1].image;
+    $scope.travelInfo =  trvInfo[1];
   }
   else if ($stateParams.id === '11223489'){
-    $scope.blah = mainSrv.travelInfo[2];
+    $scope.blah = trvInfo[2];
+    $scope.backgrnd = trvInfo[2].image;
+    $scope.travelInfo =  trvInfo[2];
   }
 });
